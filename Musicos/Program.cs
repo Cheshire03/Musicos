e797ec1e5965182baae397d8d17d765035b35550
Juan Lopez <124205724+Cheshire03@users.noreply.github.com>
2023-03-22 19:58:24 +0000

    inicio

---
+++ Musicos/Program.cs
@@ -0,0 +1,58 @@
+﻿class Musico
+{
+    public string Nombre{get;set;}
+
+    public Musico(string n)
+    {
+        Nombre=n;
+    }
+    
+    public void Saludo()
+    {
+        Console.WriteLine($"hola soy {Nombre}");
+    }
+    public virtual void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su instrumento");
+    }
+}
+class Bajista:Musico
+{
+    public string Bajo{get;set;}
+
+    public Bajista(string n,string b):base(n)
+    {
+        Bajo=b;
+    }
+    public override void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su {Bajo}");
+    }
+}
+class Baterista:Musico
+{
+    public string Bateria{get;set;}
+public Baterista(string n,string bt):base(n)
+{
+    Bateria=bt;
+}
+public override void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su {Bateria}");
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Musico> Queen=new List<Musico>();
+        Queen.Add(new Musico("Freddie Mercury"));
+        Queen.Add(new Bajista("Bryan May", "Stratocaster"));
+        Queen.Add(new Baterista("Roger Taylor","Tama"));
+        foreach(Musico m in Queen)
+        {
+            m.Saludo();
+            m.Toca();
+        }
+    }
+}
